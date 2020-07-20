552ceb905f0d165b24bb12fe3660cd9090e76335
Paula Castello <pcastello@Paula-Castello.local>
2020-07-20 19:07:51 -0300

    Comercializadores

--- src/app/pages/comercializarores/comercializarores.page.ts
+++ src/app/pages/comercializarores/comercializarores.page.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComercializaroresPage implements OnInit {
 
+  comercios: { img: string, name: string, desc: string, adress: string }[] = [
+    {
+      img: './assets/user-6.svg',
+      name: 'Carlos',
+      desc: 'Verduleria',
+      adress: 'Uspallata 3160, Parque Patricios'
+    },
+    {
+      img: './assets/user-6.svg',
+      name: 'Jose',
+      desc: 'Carniceria',
+      adress: 'Uspallata 3160, Parque Patricios'
+    },
+    {
+      img: './assets/user-6.svg',
+      name: 'Manuel',
+      desc: 'Almacen',
+      adress: 'Uspallata 3160, Parque Patricios'
+    },
+    {
+      img: './assets/user-6.svg',
+      name: 'Lucas',
+      desc: 'Heladeria',
+      adress: 'Uspallata 3160, Parque Patricios'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
