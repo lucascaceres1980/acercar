612fa52a22235c20b3b14ba3e72116701677e6a5
Paula Castello <pcastello@Paula-Castello.local>
2020-07-27 14:01:52 -0300

    Pantalla productos

--- src/app/pages/comercializadores/comercializadores.page.ts
+++ src/app/pages/comercializadores/comercializadores.page.ts
@@ -9,25 +9,25 @@ export class ComercializadoresPage implements OnInit {
 
   comercios: { img: string, name: string, desc: string, adress: string }[] = [
     {
-      img: './assets/user-6.svg',
+      img: './assets/comercializadores/user-6.svg',
       name: 'Carlos',
       desc: 'Verduleria',
       adress: 'Uspallata 3160, Parque Patricios'
     },
     {
-      img: './assets/user-6.svg',
+      img: './assets/comercializadores/user-6.svg',
       name: 'Jose',
       desc: 'Carniceria',
       adress: 'Uspallata 3160, Parque Patricios'
     },
     {
-      img: './assets/user-6.svg',
+      img: './assets/comercializadores/user-6.svg',
       name: 'Manuel',
       desc: 'Almacen',
       adress: 'Uspallata 3160, Parque Patricios'
     },
     {
-      img: './assets/user-6.svg',
+      img: './assets/comercializadores/user-6.svg',
       name: 'Lucas',
       desc: 'Heladeria',
       adress: 'Uspallata 3160, Parque Patricios'
--- src/app/pages/productos/productos.page.ts
+++ src/app/pages/productos/productos.page.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductosPage implements OnInit {
 
+  productos: { icon: string, name: string, desc: string, redirectTo: string, class: string }[] = [
+    {
+      icon: './assets/productos/apple.svg',
+      name: 'Frutas',
+      desc: 'Verdulerias por la zona',
+      redirectTo: '/frutas',
+      class: "frutas"
+    },
+    {
+      icon: './assets/productos/tomato.svg',
+      name: 'Verduras',
+      desc: 'Verdulerias por la zona',
+      redirectTo: '/verduras',
+      class: "verduras"
+    },
+    {
+      icon: './assets/productos/steak.svg',
+      name: 'Carnes',
+      desc: 'Carnicerias y pollerias por la zona',
+      redirectTo: '/carnes',
+      class: "carnes"
+    },
+    {
+      icon: './assets/productos/fish.svg',
+      name: 'Pescados',
+      desc: 'Pescaderias por la zona',
+      redirectTo: '/pescados',
+      class: "pescados"
+    }
+  ];
+  
   constructor() { }
 
   ngOnInit() {
